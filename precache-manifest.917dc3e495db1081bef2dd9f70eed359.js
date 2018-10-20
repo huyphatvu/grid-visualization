46243f2c3488e3691866508dd7cc5403513189be
huyphatvu <16vuh5@gmail.com>
2018-10-20 11:40:56 -0500

    Updates

---
+++ precache-manifest.917dc3e495db1081bef2dd9f70eed359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "575b5cb9f5abadee0573",
+    "url": "/myapp/static/js/main.575b5cb9.chunk.js"
+  },
+  {
+    "revision": "7268bd7543b576ee0803",
+    "url": "/myapp/static/js/1.7268bd75.chunk.js"
+  },
+  {
+    "revision": "575b5cb9f5abadee0573",
+    "url": "/myapp/static/css/main.e83ad697.chunk.css"
+  },
+  {
+    "revision": "2dfbf96d7632b040507e530b16943b1d",
+    "url": "/myapp/index.html"
+  }
+];
