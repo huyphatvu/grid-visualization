65c48662f6680ef1d59bc21aa6a56db57cd8795d
huyphatvu <16vuh5@gmail.com>
2018-12-17 13:50:53 -0600

    Added Dark Rose Theme

--- src/components/DemoBox/components/GridScript/GridScript.js
+++ src/components/DemoBox/components/GridScript/GridScript.js
@@ -2,29 +2,49 @@ import React, { Component } from 'react';
 import TabBar from './TabBar';
 import { connect } from 'react-redux'
 import BoxContent from '../../../../json/BoxContent.json'
+import src_css from "highlight.js/styles/atom-one-dark-reasonable.css"
+import Highlight from 'react-highlight'
+
 class GridBox extends Component {
   state = {
     selectedCodeContent: "CSS"
   }
 
+
   buttonSelectCallBack = (contentID) => {
     this.setState({selectedCodeContent: contentID})
   }
 
+  getHighlightedContents = (name, raw_content) => {
+    return(
+      <Highlight className= {'language-'+name}>
+        {raw_content}
+      </Highlight>
+    )
+  }
+
   renderCodeContent = () => {
     if (this.props.contentScript && this.props.triggeredDemoID === this.props.demoID){
-      // console.log(this.props.triggeredDemoID[0])
-      return (this.state.selectedCodeContent === "HTML") ? <pre>{this.props.contentScript.html}</pre> : <pre>{this.props.contentScript.css}</pre>
+      if ( this.state.selectedCodeContent === "HTML"){
+        return this.getHighlightedContents("html", this.props.contentScript.html)
+      } else {
+        return this.getHighlightedContents("css", this.props.contentScript.css)
+      }
     } else {
-      return (this.state.selectedCodeContent === "HTML") ?<pre>{BoxContent.BoxContentArr[this.props.demoID][0].html}</pre> :  <pre>{BoxContent.BoxContentArr[this.props.demoID][0].css}</pre>
+      if (this.state.selectedCodeContent === "HTML"){
+        return this.getHighlightedContents("html", BoxContent.BoxContentArr[this.props.demoID][0].html)
+      } else {
+        return this.getHighlightedContents("css", BoxContent.BoxContentArr[this.props.demoID][0].css)
+      }
     }
   }
+
  // BoxContent.BoxContentArr[0][0]
   render(){
     return(
         <div className="script-block">
           <TabBar callbackFromParent={this.buttonSelectCallBack} tabID={this.props.scriptID} />
-          <div className="code-content">
+          <div className="code-content" src={src_css}>
             {this.renderCodeContent()}
           </div>
         </div>
